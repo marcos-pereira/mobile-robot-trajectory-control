3fd684359d883dd9f87f86d8a23b82075f98999e
Marcos Pereira <marcos.si.pereira@gmail.com>
2023-09-09 05:17:18 +0100

    [main.py] Add functions that align robot with a goal pose. - Add function to align with goal. - Add function to convert twist to whell velocities. - Create loop to align robot with goal using synchronous mode.

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -5,6 +5,63 @@ import math
 
 from coppeliasim_zmqremoteapi_client import *
 
+def align_with_goal(xd, yd, xm, ym, theta_m, desired_error):
+    """Return the twist to apply to robot such that it aligns with the 
+    desired position (xd, yd) given the measured position (xm, ym), and
+    the measured orientation about the z-axis (theta_m).
+
+    Args:
+        xd (float): the desired x position.
+        yd (float): the desired y position.
+        xm (float): the measured x position.
+        ym (float): the measured y position.
+        theta_m (float): the measured orientation about the z-axis.
+        desired_error (float): the desired alignment error to reach.
+    """
+    # Get current alignment angle
+    delta_x = xd - xm
+    delta_y = yd - ym
+    theta_ref = np.arctan2(delta_y,delta_x)
+
+    # The linear velocity is zero, since we are aligning
+    V = 0
+
+    # The angular velocity is the difference to align
+    Wz = theta_ref - theta_m
+
+    # Store twist
+    twist = np.array([[V],[Wz]])
+        
+    # If not aligned, return 0
+    aligned = False
+    if abs(theta_ref - theta_m) < desired_error:
+        # If within error tolerance to align
+        aligned = True
+
+    return(twist, aligned)
+
+def twist_to_wheel_velocities(twist, wheel_radius=0.195/2.0, l_shaft=0.33):
+    """Return the wheel velocities for a given twist.
+
+    Args:
+        twist (_type_): the desired twist
+        wheel_radius (float, optional): the wheel radius. Defaults to 0.195/2.0.
+        l_shaft (float, optional): the length of the shaft between wheels. Defaults to 0.33.
+
+    Returns:
+        np array: the right and left wheel velocities.
+    """
+    # Transform wheel velocities to twist 
+    T_wheel_to_twist = np.array([[1.0/2.0, 1.0/2.0], [1.0/l_shaft, -1.0/l_shaft]])
+
+    # Convert from twist to wheel velocities
+    wheel_velocities = np.matmul(np.linalg.inv(T_wheel_to_twist), twist)
+    
+    # Convert from linear velocity to angular velocity
+    angular_wheel_velocities = wheel_velocities/wheel_radius
+
+    return (angular_wheel_velocities)
+
 def main():
     # Sampling time [seconds]
     sampling_time = 0.005
@@ -107,17 +164,42 @@ def main():
         xdot_trajectory = np.append(xdot_trajectory,x_dot)
         ydot_trajectory = np.append(ydot_trajectory,y_dot)
         
-    # create a client to connect to zmqRemoteApi server:
-    # (creation arguments can specify different host/port,
-    # defaults are host='localhost', port=23000)
     client = RemoteAPIClient()
-    
-    # get a remote object:
-    sim = client.getObject('sim')
-    
+    sim = client.getObject('sim')    
     client.setStepping(True)
     
     sim.startSimulation()
+    
+    desired_alignment_error = 0.01
+    
+    robot_handle = sim.getObject("/Pioneer_p3dx_connection4")
+    right_motor_handle = sim.getObject("/Pioneer_p3dx_rightMotor")
+    left_motor_handle = sim.getObject("/Pioneer_p3dx_leftMotor")
+    
+    while True:
+        robot_pose = sim.getObjectPosition(robot_handle, sim.handle_world)
+        robot_orientation = sim.getObjectOrientation(robot_handle, sim.handle_world)
+        
+        (twist_to_align, aligned) = align_with_goal(
+            x_trajectory[0], 
+            y_trajectory[0], 
+            robot_pose[0], 
+            robot_pose[1], 
+            robot_orientation[2], 
+            desired_alignment_error)
+        
+        wheel_velocities = twist_to_wheel_velocities(twist_to_align)
+    
+        sim.setJointTargetVelocity(right_motor_handle, wheel_velocities[0][0])
+        sim.setJointTargetVelocity(left_motor_handle, wheel_velocities[1][0])
+        
+        client.step()
+        
+        if aligned is True:
+            break
+        
+    sim.stopSimulation()
+        
 
 if __name__ == '__main__':
     main()
